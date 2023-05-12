bdb70ec721b63124fef8d8367f9038e989a43f87
Preston Ellis <prestone03@gmail.com>
2023-05-12 09:38:24 -0700

    bug fix ratings

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -133,7 +133,7 @@ class RecipeCard extends HTMLElement {
   <p class="organization">${data.organization}</p>
   <div class="rating">
     <span>${data.rating}</span>
-    <img src="/assets/images/icons/${data.rating}-star.svg" alt="${data.rating} star${data.rating == 1?'':'s'}">
+    <img src="assets/images/icons/${data.rating}-star.svg" alt="${data.rating} star${data.rating == 1?'':'s'}">
     <span>(${data.numRatings})</span>
   </div>
   <time>${data.lengthTime}</time>
